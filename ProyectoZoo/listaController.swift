44a084d52d4b8dace27e9fa56e67607db18988ae
José Trejo <joseatrejos>
2020-10-20 16:12:16 -0700

    proyecto

---
+++ ProyectoZoo/listaController.swift
@@ -0,0 +1,21 @@
+//
+//  listaController.swift
+//  ProyectoZoo
+//
+//  Created by Alumno on 10/20/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class listaController : UIViewController{
+    
+    @IBAction func doAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)    }
+    
+    @IBAction func doAction2(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ ProyectoZoo/principalController.swift
@@ -0,0 +1,20 @@
+//
+//  principalController.swift
+//  ProyectoZoo
+//
+//  Created by Alumno on 10/20/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class principalController : UIViewController{
+
+    
+    
+    @IBAction func doAction(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)    }
+    
+}
